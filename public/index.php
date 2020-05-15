ba6b04abe3772baa43cb2466629e67a2f7b567ac
daniele cirrincione <rudi1cd@gmail.com>
2020-05-15 01:37:26 +0100

    Adds log

--- public/index.php
+++ public/index.php
@@ -11,9 +11,9 @@ header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers
 //this is global so i can change the url at later date
 $GLOBALS['uri'] = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $GLOBALS['uri'] = explode( '/', $GLOBALS['uri'] );
+echo json_encode($GLOBALS['uri']);
 
 if($GLOBALS['uri'][4] !== 'api' && $GLOBALS['uri'][5] !== 'flyers' ){
-  echo $GLOBALS['uri'][2]; echo $GLOBALS['uri'][3];echo $GLOBALS['uri'][4]; echo $GLOBALS['uri'][4];
   header("HTTP/1.1 404 Not Found");
   exit();
 }
