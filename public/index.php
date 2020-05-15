2666e84ae9ac1f8aa22dd69d5eeea78939354c12
daniele cirrincione <rudi1cd@gmail.com>
2020-05-15 14:22:51 +0100

    more changes

--- public/index.php
+++ public/index.php
@@ -8,12 +8,12 @@ header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
-//this is global so i can change the url at later date
-$GLOBALS['uri'] = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-$GLOBALS['uri'] = explode( '/', $GLOBALS['uri'] );
-echo "<script>console.log('" .  $_SERVER['REQUEST_URI'] . "');</script>";
+//make this is global tochange url at later date
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$uri = explode( '/', $GLOBALS['uri'] );
+echo "<script>console.log('" .  $uri . "');</script>";
 
-if($GLOBALS['uri'][0] !== 'api' && $GLOBALS['uri'][1] !== 'flyers' ){
+if($uri[0] !== 'api' && $uri[1] !== 'flyers' ){
   header("HTTP/1.1 404 Not Found");
   exit();
 }
