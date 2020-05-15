f61629d8aaccb5f1ac3b08f6f2206c2dbaca383c
daniele cirrincione <rudi1cd@gmail.com>
2020-05-15 14:20:11 +0100

    console log

--- public/index.php
+++ public/index.php
@@ -11,7 +11,7 @@ header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers
 //this is global so i can change the url at later date
 $GLOBALS['uri'] = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $GLOBALS['uri'] = explode( '/', $GLOBALS['uri'] );
-echo "<script>console.log('" . json_encode( $_SERVER['REQUEST_URI']) . "');</script>";
+echo "<script>console.log('" .  $_SERVER['REQUEST_URI'] . "');</script>";
 
 if($GLOBALS['uri'][0] !== 'api' && $GLOBALS['uri'][1] !== 'flyers' ){
   header("HTTP/1.1 404 Not Found");
